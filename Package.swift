09efd49eb1ede0e887db25122fb6bb1266204f2c
Robert Deans <robert.deans@uk.ibm.com>
2016-09-30 13:41:34 +0100

    Kitura/#716 Added sample tests

--- Package.swift
+++ Package.swift
@@ -18,12 +18,6 @@ import PackageDescription
 
 let package = Package(
     name: "KituraSample",
-    targets: [
-        Target(
-            name: "KituraSample",
-            dependencies: []
-        )
-    ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+@testable import KituraSampleTests
+
+XCTMain([
+           testCase(KituraSampleTests.allTests)
+       ])
