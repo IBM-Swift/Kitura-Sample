8a0ce5491d46e9ebba95c13804762173403346cd
andy <andrew.lees11@ibm.com>
2018-09-27 13:25:56 +0100

    removed added whitespace

--- Sources/Application/Routes/MarkdownRoutes.swift
+++ Sources/Application/Routes/MarkdownRoutes.swift
@@ -19,17 +19,17 @@ import KituraMarkdown
 func initializeMarkdownRoutes(app: App) {
     // Add KituraMarkdown as a TemplateEngine
     app.router.add(templateEngine: KituraMarkdown())
-    
+
     app.router.setDefault(templateEngine: KituraMarkdown())
     
-    
+
     app.router.get("/docs") { _, response, next in
         response.headers["Content-Type"] = "text/html"
         try response.render("/docs/index.md", context: [String:Any]())
         try response.status(.OK).end()
-        
+
     }
-    
+
     app.router.get("/docs/*") { request, response, next in
         response.headers["Content-Type"] = "text/html"
         if request.urlURL.path == "/docs" {
